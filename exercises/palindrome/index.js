e17609072b3d35e520463c574b9ccde791219586
Isiah VanBrackle <squarepheonix@gmail.com>
2019-03-27 12:58:34 -0400

    made use of .every() to solve palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -6,11 +6,18 @@
 // --- Examples:
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
+// study the every function more.
 
 function palindrome(str) {
   const reversed = str.split('').reverse().join('');
   // take the user's string split it into different characters, reverse their order and rejoin them.
   return str === reversed; // then return the boolean value (true or false )
 }
+
+// function everyPalindrome(str) { // every is an array helper method which checks if every value in an array passes a test(provided as a function)
+//   return str.split('').every((character, i) =>{ // the function starts at 0, we include a second argument of i so that we can acces the index
+//     return character === str[str.length - i -1] // it's known that str.length-1 gives you the last value in an array. by subtracting the current index first we allow ourselves to 'increment' down the array.
+//   })
+// }
 
 module.exports = palindrome;
